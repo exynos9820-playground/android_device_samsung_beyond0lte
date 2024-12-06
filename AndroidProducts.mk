8f7a6d3259ab635645e763669565325ee5be94fe
igor121 <chernokalsky89@gmail.com>
2024-12-06 13:04:39 +0300

    lineage > YAAP

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_beyond0lte.mk
+    $(LOCAL_DIR)/yaap_beyond0lte.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_beyond0lte-eng \
-    lineage_beyond0lte-user \
-    lineage_beyond0lte-userdebug
+    yaap_beyond0lte-eng \
+    yaap_beyond0lte-user \
+    yaap_beyond0lte-userdebug
